fb93eea05d2cb46482693ab794b0dc7b676a8aaf
Sadek Drobi <github@sadekdrobi.com>
2012-03-15 22:07:18 +0100

    fix erring promise chaining

--- framework/src/play/src/main/scala/play/api/libs/concurrent/Promise.scala
+++ framework/src/play/src/main/scala/play/api/libs/concurrent/Promise.scala
@@ -195,7 +195,12 @@ class STMPromise[A] extends Promise[A] with Redeemable[A] {
     val result = new STMPromise[B]()
     this.addAction(p => p.value match {
       case Redeemed(a) =>
-       (try{ f(a)}catch{case e => {println("wow! "+e); throw e}}).extend(ip => ip.value match {
+       (try{ 
+         f(a)
+       } catch{
+         case e =>
+           Promise.pure[B](throw e)
+       }).extend(ip => ip.value match {
           case Redeemed(a) => result.redeem(a)
           case Thrown(e) => result.redeem(throw e)
 
@@ -249,7 +254,7 @@ object PurePromise {
 
 object Promise {
 
-  def pure[A](a: A): Promise[A] = PurePromise(a)
+  def pure[A](a: => A): Promise[A] = PurePromise(a)
 
   def apply[A](): Promise[A] with Redeemable[A] = new STMPromise[A]()
 
